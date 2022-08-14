9144496c010e0fb7bff1f0709cec4e4f05f80d35
MurodjonT <murodjon.mmm.99@mail.ru>
2022-08-14 10:16:12 +0500

    Some places changed, and this is ready tarnslate bot for users

--- bot.py
+++ bot.py
@@ -1,3 +1,121 @@
+# from googletrans import Translator
+# # print(tr.translate('siz yaxshimisiz?', dest = 'en'))
+# from buttons import til
+# import logging
+# import sqlite3
+# from api import API_TOKEN
+# from aiogram import Bot, Dispatcher, executor, types
+# from aiogram.types import CallbackQuery
+# from db import Sql
+# from datetime import datetime
+# import pytz
+
+
+# # country_time_zone = pytz.timezone('America/New_York')
+# # country_time = datetime.now(country_time_zone)
+# # print(country_time.strftime("Date is %d-%m-%y and time is %H:%M:%S"))
+
+
+
+# tr = Translator()
+
+# # Configure logging
+# logging.basicConfig(level=logging.INFO)
+
+# #Initialize bot and dispatcher
+# bot = Bot(token=API_TOKEN)
+# dp = Dispatcher(bot)
+
+
+
+# x = Sql()
+
+# @dp.message_handler(commands=['start', 'help'])
+# async def send_welcome(message: types.Message):
+# 	x.baza_create()
+#     user_id = message.from_user.id
+#     username = message.from_user.username 
+#     first_name = message.from_user.first_name 
+#     y = x.user_check(user_id)
+#     if y is None:
+#         x.user_add(user_id, firstname, username)
+#         await bot.send_message(1813994648, f"User ID: {user_id}\nName: {firstname}\nUsername: @{username}")
+#         await message.answer("Assalamu alaykum \nHush kelibsiz!\nSiz bu bot orqali nafaqat tarjima \nbalki tarjima qilayotgan davlatingizni vaqtlarini ham ko'rish imkoniyati bor!")
+#         await message.answer("Marhamat so'z yoki matn kiriting!")
+#     else:
+#         await message.answer("Qaytganingiz bilan!\nSiz bu bot orqali nafaqat tarjima \nbalki tarjima qilayotgan davlatingizni vaqtlarini ham ko'rish imkoniyati bor!")
+#         await message.answer("Marhamat so'z yoki matn kiriting!" )
+
+
+
+# @dp.message_handler()
+# async def echo(message: types.Message):
+# 	global t
+# 	t = message.text
+# 	await message.reply('Qaysi tilga tarjima qilmoqchisiz?', reply_markup = til)
+
+# @dp.callback_query_handler(text = "ru")
+# async def tarjima(call: CallbackQuery):
+
+# 	country_time_zone = pytz.timezone('Europe/Moscow')
+# 	country_time = datetime.now(country_time_zone)
+# 	# print(country_time.strftime("Date is %d-%m-%y and time is %H:%M:%S"))
+# 	tarjima = tr.translate(t, dest = "ru")
+# 	await call.message.answer(country_time)
+# 	await call.message.reply(tarjima.text)
+
+# @dp.callback_query_handler(text = "en")
+# async def tarjima(call: CallbackQuery):
+
+# 	country_time_zone = pytz.timezone('America/New_York')
+# 	country_time = datetime.now(country_time_zone)
+# 	tarjima = tr.translate(t, dest = "en")
+# 	await call.message.answer(country_time)
+# 	await call.message.reply(tarjima.text)
+
+# @dp.callback_query_handler(text = "uz")
+# async def tarjima(call: CallbackQuery):
+
+# 	country_time_zone = pytz.timezone('Asia/Tashkent')
+# 	country_time = datetime.now(country_time_zone)
+# 	tarjima = tr.translate(t, dest = "uz")
+# 	await call.message.answer(country_time)
+# 	await call.message.reply(tarjima.text)
+
+
+
+# @dp.callback_query_handler(text = "fr")
+# async def tarjima(call: CallbackQuery):
+
+# 	country_time_zone = pytz.timezone('Europe/Paris')
+# 	country_time = datetime.now(country_time_zone)
+# 	tarjima = tr.translate(t, dest = "fr")
+# 	await call.message.answer(country_time)
+# 	await call.message.reply(tarjima.text)
+
+
+# @dp.callback_query_handler(text = "ar")
+# async def tarjima(call: CallbackQuery):
+
+# 	country_time_zone = pytz.timezone('Asia/Dubai')
+# 	country_time = datetime.now(country_time_zone)
+# 	tarjima = tr.translate(t, dest = "ar")
+# 	await call.message.answer(country_time)
+# 	await call.message.reply(tarjima.text)
+
+
+# @dp.callback_query_handler(text = "zh-cn")
+# async def tarjima(call: CallbackQuery):
+
+# 	country_time_zone = pytz.timezone('Asia/Shanghai')
+# 	country_time = datetime.now(country_time_zone)
+# 	tarjima = tr.translate(t, dest = "zh-cn")
+# 	await call.message.answer(country_time)
+# 	await call.message.reply(tarjima.text)
+
+# if __name__ == '__main__':
+#     executor.start_polling(dp, skip_updates=True)
+
 from googletrans import Translator
 # print(tr.translate('siz yaxshimisiz?', dest = 'en'))
 from buttons import til
@@ -6,7 +124,7 @@ import sqlite3
 from api import API_TOKEN
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import CallbackQuery
-from db import Sql
+from db import Translate
 from datetime import datetime
 import pytz
 
@@ -28,25 +146,24 @@ dp = Dispatcher(bot)
 
 
 
-x = Sql()
+x = Translate()
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-	x.baza_create()
+    x.create_table()
     user_id = message.from_user.id
-    username = message.from_user.username 
-    first_name = message.from_user.first_name 
-    y = x.user_check(user_id)
+    firstname = message.from_user.first_name
+    username = message.from_user.username
+    
+    y = x.user_exist(user_id)
     if y is None:
-        x.user_add(user_id, firstname, username)
-        await bot.send_message(1813994648, f"User ID: {user_id}\nName: {firstname}\nUsername: @{username}")
-        await message.answer("Assalamu alaykum \nHush kelibsiz!\nSiz bu bot orqali nafaqat tarjima \nbalki tarjima qilayotgan davlatingizni vaqtlarini ham ko'rish imkoniyati bor!")
-        await message.answer("Marhamat so'z yoki matn kiriting!")
+        x.add_user(user_id, firstname, username)
+        await bot.send_message(614605333, f"Tashrif buyurishdi! \nUser ID: {user_id}\nName: {firstname}\nUsername: @{username}")
+        await message.answer(f"Здравствуйте @{message.chat.username}!\nДобро пожаловать на @perevodcheek_bot")
+        await message.answer("Введите текст:" )
     else:
-        await message.answer("Qaytganingiz bilan!\nSiz bu bot orqali nafaqat tarjima \nbalki tarjima qilayotgan davlatingizni vaqtlarini ham ko'rish imkoniyati bor!")
-        await message.answer("Marhamat so'z yoki matn kiriting!" )
-
-
+        await message.answer(f"С возвращением @{message.chat.username}!\nДобро пожаловать на @perevodcheek_bot")
+        await message.answer("Введите текст:" )
 
 @dp.message_handler()
 async def echo(message: types.Message):
--- db.py
+++ db.py
@@ -1,29 +1,51 @@
-import sqlite3
+# import sqlite3
 
 
-class Sql:
-	def __init__(self):
-		self.connection = sqlite3.connect('bot.db')
-		self.cursor = self.connection.cursor()
+# class Sql:
+# 	def __init__(self):
+# 		self.connection = sqlite3.connect('bot.db')
+# 		self.cursor = self.connection.cursor()
 	
 
 
-	def baza_create(self):
-		self.cursor.execute("""CREATE TABLE IF not exists user(
-        id integer,
-        firstname varchar(24),
-        username varchar(24),
-        tel integer NULL
-		)""")
+# 	def baza_create(self):
+# 		self.cursor.execute("""CREATE TABLE IF not exists user(
+#         id integer,
+#         firstname varchar(24),
+#         username varchar(24),
+#         tel integer NULL
+# 		)""")
 
 
 
-	def user_add(self, idi, username, first_name):
-		self.cursor.execute("INSERT INTO user VALUES ({}, '{}', '{}', NULL)".format(idi, username, first_name))
-        return self.connection.commit()
+# 	def user_add(self, idi, username, first_name):
+# 		self.cursor.execute("INSERT INTO user VALUES ({}, '{}', '{}', NULL)".format(idi, username, first_name))
+#         return self.connection.commit()
     
-    def user_check(self, user_id):
-        self.cursor.execute(f"SELECT * FROM user WHERE id = {user_id}")
+#     def user_check(self, user_id):
+#         self.cursor.execute(f"SELECT * FROM user WHERE id = {user_id}")
+#         data = self.cursor.fetchone()
+#         return data
+
+
+
+import sqlite3
+
+class Translate():
+    def __init__(self):
+        self.connection = sqlite3.connect('bot.db')
+        self.cursor = self.connection.cursor()
+    def create_table(self):
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS users(
+            user_id integer,
+            first_name varchar(65),
+            user_name varchar(65)
+        )""")
+    def add_user(self,user_id,firstname, username):
+        self.cursor.execute("INSERT INTO users VALUES ({},'{}','{}')".format(user_id, firstname, username))
+        self.connection.commit()
+    def user_exist(self, userid):
+        self.cursor.execute(f"SELECT user_id FROM users WHERE user_id = {userid}")
         data = self.cursor.fetchone()
         return data
 
